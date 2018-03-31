53725db4b221e212d03c36cc0988c6c55342e6e7
Derek <DerekCDaley@gmail.com>
2018-03-30 23:35:28 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  key = '41e9d8e015a077b664b1b77d2e50476d';
+  marvels:any = [];
+  constructor(private http: HttpClient){
+  }
+
+  ngOnInit(): void {
+  }
+
+  search(){
+    let that = this;
+    this.http.get('https://gateway.marvel.com/v1/public/characters?apikey=' + this.key).subscribe(data => {
+      that.marvels = data.data.results;
+    });
+  }
+
+  // search(){
+  //   let that = this;
+  //   this.http.get('https://random.dog/doggos').subscribe(data => {
+  //     console.log(data);
+  //     that.marvels = data;
+  //   });
+  // }
+
+  getSuperHeros(id){
+    let that = this;
+    console.log(id);
+    this.http.get(`https://gateway.marvel.com/v1/public/characters/${id}?apikey=${this.key}`).subscribe(data => {
+      that.marvels = data.data.results;
+    });
+  }
+
 }
